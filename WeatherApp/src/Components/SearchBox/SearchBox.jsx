691cab051a7e53b9d987a52fe8056c5025d7ca8b
@123umair <khanuumair91@gmail.com>
2025-11-24 12:35:47 +0500

    using fetch to handle the API data

--- WeatherApp/src/Components/SearchBox/SearchBox.jsx
+++ WeatherApp/src/Components/SearchBox/SearchBox.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
+import axios from 'axios';
 const SearchBox = ({ updateInfo, setError }) => {
   const [city, setCity] = useState("")
   const API_KEY = 'a63cdedea394f2f1cb5f46105c8addb6'
   const url = 'https://api.openweathermap.org/data/2.5/weather'
   const checkWeather = async () => {
     try {
-      let resp = await fetch(`${url}?q=${city}&appid=${API_KEY}&units=metric`)
-      let jsonResp = await resp.json()
+      // let resp = await fetch(`${url}?q=${city}&appid=${API_KEY}&units=metric`)
+      // let jsonResp = await resp.json() // this api with fetch method 
+       let resp = await axios.get(`${url}?q=${city}&appid=${API_KEY}&units=metric`)
+       let jsonResp = resp.data
+
 
       let Result = {
         city: city,
